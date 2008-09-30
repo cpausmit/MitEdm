59a39575a9d83200a3e4d81c15a471f5f8bf39a9
mrudolph <>
2008-09-30 08:53:40 +0000

    cfi for V producer module

---
+++ Producers/python/v2ss_cfi.py
@@ -0,0 +1,11 @@
+import FWCore.ParameterSet.Config as cms
+
+v2ss = cms.EDProducer("ProducerV2SS",
+                      iStables1 = cms.untracked.string('PisStable'),
+                      iStables2 = cms.untracked.string('PisStable'),
+                      oPid = cms.untracked.int32(310),
+                      minRadius = cms.untracked.double(0.1),
+                      minMass = cms.untracked.double(0.350),
+                      maxMass = cms.untracked.double(0.650),
+                      maxZDistance = cms.untracked.double(1.0)
+                      )
