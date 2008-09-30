b3750f57a2bebd129aedb8e7e1d1f536a188cf16
mrudolph <>
2008-09-30 10:02:17 +0000

    Sequence to produce Ksh and Lambda w/o refitting proton tracks.

---
+++ Producers/python/vProducerNoRefit_cff.py
@@ -0,0 +1,20 @@
+import FWCore.ParameterSet.Config as cms
+
+import MitEdm.Producers.stableParts_cfi
+
+PisStable = MitEdm.Producers.stableParts_cfi.stableParts.clone()
+
+ProtonsStable = MitEdm.Producers.stableParts_cfi.stableParts.clone()
+ProtonsStable.oPid = cms.untracked.int32(2212)
+
+import MitEdm.Producers.v2ss_cfi
+Ksh2PiPi = MitEdm.Producers.v2ss_cfi.v2ss.clone()
+
+Lambda2ProtPi = MitEdm.Producers.v2ss_cfi.v2ss.clone()
+Lambda2ProtPi.iStables2 = cms.untracked.string('ProtonsStable')
+Lambda2ProtPi.oPid= cms.untracked.int32(3122)
+Lambda2ProtPi.minMass = cms.untracked.double(1.0)
+Lambda2ProtPi.maxMass = cms.untracked.double(1.3)
+
+
+vProducer = cms.Sequence(PisStable*TrackRefitter*ProtonsStable*Ksh2PiPi*Lambda2ProtPi)
