c2ef701e4e352e5beb2f490f72f00d7800472f8a
bendavid <>
2008-09-30 20:12:10 +0000

    Added conversion filler sequence suitable for production

--- Producers/python/conversionProducer_cff.py
+++ Producers/python/conversionProducer_cff.py
@@ -9,3 +9,23 @@ stableParts.oPid = 11
 mvfConversions.rhoMin = 1.0
 
 conversionProducer = cms.Sequence(stableParts*mvfConversions)
+
+conversionFiller = cms.EDAnalyzer("FillMitTree",
+    defactive = cms.untracked.bool(False),
+    
+    StableParts = cms.untracked.PSet(
+      active       = cms.untracked.bool(True),
+      mitName      = cms.untracked.string('ElectronsStable'),
+      edmName      = cms.untracked.string('stableParts'),
+      trackMapName = cms.untracked.string('TracksMapName'),
+      basePartMap  = cms.untracked.string('ElectronsStableMapName')
+    ),
+
+    DecayParts = cms.untracked.PSet(
+      active       = cms.untracked.bool(True),
+      mitName      = cms.untracked.string('MvfConversions'),
+      edmName      = cms.untracked.string('mvfConversions'),
+      vertexMap    = cms.untracked.string('PrimaryVertexBSMap'),
+      basePartMaps = cms.untracked.vstring('ElectronsStableMapName')
+    )
+)
