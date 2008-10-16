12cfb5325a53478c7c056ba3509b9c1132db8cff
bendavid <>
2008-10-16 16:46:29 +0000

    Moved transient track import to HitDropper config

--- Producers/python/conversionProducer_cff.py
+++ Producers/python/conversionProducer_cff.py
@@ -1,7 +1,5 @@
 import FWCore.ParameterSet.Config as cms
 
-from TrackingTools.TransientTrack.TransientTrackBuilder_cfi import *
-
 from MitEdm.Producers.HitDropper_cfi import *
 
 from MitEdm.Producers.stableParts_cfi import *
