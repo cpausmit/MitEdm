d41cf6bd686daa0c33a0b37ced4b8be1f6a63446
bendavid <>
2008-10-16 16:46:49 +0000

    Made setting of constraint option explicit in configuration file

--- Producers/python/conversions_cfi.py
+++ Producers/python/conversions_cfi.py
@@ -1,4 +1,4 @@
-# $Id:$
+# $Id: conversions_cfi.py,v 1.3 2008/10/03 23:53:51 loizides Exp $
 
 import FWCore.ParameterSet.Config as cms
 
@@ -8,6 +8,7 @@ mvfConversions = cms.EDProducer("ProducerConversions",
                       iPVertexes = cms.untracked.string('offlinePrimaryVerticesWithBS'),
                       oPid = cms.untracked.int32(22),
                       usePVertex = cms.untracked.bool(True),
+                      convConstraint = cms.untracked.bool(False),
                       convConstraint3D = cms.untracked.bool(True),
                       rhoMin = cms.untracked.double(0.0)
                       )
