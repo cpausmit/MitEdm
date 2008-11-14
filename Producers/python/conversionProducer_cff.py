8ac7b80a101412fbf01c79d7a9583de6d4bcdf3f
bendavid <>
2008-11-14 16:26:04 +0000

    slightly loosened preselection cut

--- Producers/python/conversionProducer_cff.py
+++ Producers/python/conversionProducer_cff.py
@@ -8,7 +8,7 @@ from MitEdm.Producers.conversions_cfi import *
 
 #stableParts.iTracks = 'ckfOutInTracksFromConversions'
 stableParts.oPid = 11
-mvfConversions.rhoMin = 1.0
+mvfConversions.rhoMin = 0.9
 
 conversionProducer = cms.Sequence(stableParts*mvfConversions)
 
