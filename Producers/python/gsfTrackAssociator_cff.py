1117dd6e8eb0585abdb44d372d4f3400049ff6fd
bendavid <>
2008-12-01 17:25:09 +0000

    Remove unused configuration option

--- Producers/python/gsfTrackAssociator_cff.py
+++ Producers/python/gsfTrackAssociator_cff.py
@@ -1,9 +1,8 @@
-# $Id: conversions_cfi.py,v 1.4 2008/10/16 16:46:49 bendavid Exp $
+# $Id: gsfTrackAssociator_cff.py,v 1.1 2008/11/04 19:25:56 bendavid Exp $
 
 import FWCore.ParameterSet.Config as cms
 
 gsfTrackAssociator = cms.EDProducer("TrackToTrackAssociator",
                       fromTracks      = cms.untracked.string('pixelMatchGsfFit'),
-                      toTracks        = cms.untracked.string('generalTracks'),
-                      associationName = cms.untracked.string('gsfToTrackAssociation')
+                      toTracks        = cms.untracked.string('generalTracks')
                       )
