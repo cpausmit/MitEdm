b65613f36af1d59fd10eb9a8d5eb6ff56e1ee357
bendavid <>
2009-03-20 20:42:19 +0000

    Changed default edm collection and sequence names

--- Producers/python/conversionProducerGsf_cff.py
+++ Producers/python/conversionProducerGsf_cff.py
@@ -1,23 +1,23 @@
-# $Id: conversionProducerGsf_cff.py,v 1.3 2009/03/16 22:32:44 bendavid Exp $
+# $Id: conversionProducerGsf_cff.py,v 1.4 2009/03/17 14:26:24 loizides Exp $
 
 import FWCore.ParameterSet.Config as cms
 
 from MitEdm.Producers.HitDropper_cfi import *
 
 import MitEdm.Producers.stableParts_cfi
-stablePartsGsf = MitEdm.Producers.stableParts_cfi.stableParts.clone()
+stablePartsGeneralGsf = MitEdm.Producers.stableParts_cfi.stableParts.clone()
 
 from MitEdm.Producers.conversions_cfi import *
 mvfConversionsGsf = MitEdm.Producers.conversions_cfi.mvfConversions.clone()
 
 #stableParts.iTracks = 'ckfOutInTracksFromConversions'
-stablePartsGsf.iTracks = 'generalTracksGsf'
-stablePartsGsf.oPid = 11
+stablePartsGeneralGsf.iTracks = 'generalTracksGsf'
+stablePartsGeneralGsf.oPid = 11
 mvfConversionsGsf.rhoMin = 0.9
 mvfConversionsGsf.iStables1 = 'stablePartsGsf'
 mvfConversionsGsf.iStables2 = 'stablePartsGsf'
 
-conversionProducerGsf = cms.Sequence(stablePartsGsf*mvfConversionsGsf)
+conversionProducerGsf = cms.Sequence(stablePartsGeneralGsf*mvfConversionsGsf)
 
 conversionFillerGsf = cms.EDAnalyzer("FillMitTree",
     fillers = cms.untracked.vstring('StableParts',
@@ -25,10 +25,10 @@ conversionFillerGsf = cms.EDAnalyzer("FillMitTree",
     
     StableParts = cms.untracked.PSet(
       active       = cms.untracked.bool(True),
-      mitName      = cms.untracked.string('ElectronsStableGsf'),
-      edmName      = cms.untracked.string('stablePartsGsf'),
+      mitName      = cms.untracked.string('ElectronsStableGeneralGsf'),
+      edmName      = cms.untracked.string('stablePartsGeneralGsf'),
       trackMapName = cms.untracked.string('GeneralGsfTracksMapName'),
-      basePartMap  = cms.untracked.string('ElectronsStableGsfMapName'),
+      basePartMap  = cms.untracked.string('ElectronsStableGeneralGsfMapName'),
       fillerType   = cms.untracked.string('FillerStableParts')
     ),
 
@@ -37,7 +37,7 @@ conversionFillerGsf = cms.EDAnalyzer("FillMitTree",
       mitName      = cms.untracked.string('MvfConversionsGsf'),
       edmName      = cms.untracked.string('mvfConversionsGsf'),
       vertexMap    = cms.untracked.string('PrimaryVertexBSMap'),
-      basePartMaps = cms.untracked.vstring('ElectronsStableGsfMapName'),
+      basePartMaps = cms.untracked.vstring('ElectronsStableGeneralGsfMapName'),
       fillerType   = cms.untracked.string('FillerDecayParts')
     )
 )
