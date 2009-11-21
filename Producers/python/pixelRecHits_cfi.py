9ba1055c26c7ad3772f07a0bcfc9e2c1906b446c
loizides <>
2009-11-21 19:37:23 +0000

    Use standard pixel reco

--- Producers/python/pixelRecHits_cfi.py
+++ Producers/python/pixelRecHits_cfi.py
@@ -1,4 +1,4 @@
-# $Id: conversionElectronsStable_cfi.py,v 1.2 2009/07/12 13:12:06 bendavid Exp $
+# $Id: pixelRecHits_cfi.py,v 1.1 2009/11/05 09:33:28 loizides Exp $
 
 import FWCore.ParameterSet.Config as cms
 
@@ -6,6 +6,6 @@ from RecoLocalTracker.SiPixelRecHits.PixelCPEESProducers_cff import *
 
 siPixelRecHits = cms.EDFilter("SiPixelRecHitConverter",
     src = cms.InputTag("siPixelClusters"),
-    CPE = cms.string('PixelCPETemplateReco'),
+#    CPE = cms.string('PixelCPETemplateReco'),
     VerboseLevel = cms.untracked.int32(0),
 )
