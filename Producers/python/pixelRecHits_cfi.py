403e4da8065ab7a991dc0b8b21b302fd78ca81c8
bendavid <>
2009-11-22 16:39:36 +0000

    Forward to producer from standard reco sequence

--- Producers/python/pixelRecHits_cfi.py
+++ Producers/python/pixelRecHits_cfi.py
@@ -1,11 +1,7 @@
-# $Id: pixelRecHits_cfi.py,v 1.1 2009/11/05 09:33:28 loizides Exp $
+# $Id: pixelRecHits_cfi.py,v 1.2 2009/11/21 19:37:23 loizides Exp $
 
 import FWCore.ParameterSet.Config as cms
 
 from RecoLocalTracker.SiPixelRecHits.PixelCPEESProducers_cff import *
 
-siPixelRecHits = cms.EDFilter("SiPixelRecHitConverter",
-    src = cms.InputTag("siPixelClusters"),
-#    CPE = cms.string('PixelCPETemplateReco'),
-    VerboseLevel = cms.untracked.int32(0),
-)
+from RecoLocalTracker.SiPixelRecHits.SiPixelRecHits_cfi import *
