49db1dd632399bb5b6c2921abdf597ef6ccb90d6
loizides <>
2009-12-02 15:42:29 +0000

    CLeaned

--- Analyzers/src/None.cc
+++ Analyzers/src/None.cc
@@ -1,4 +1,12 @@
-// $Id$
+//--------------------------------------------------------------------------------------------------
+// $Id: ProducerConversions.h,v 1.5 2009/07/15 20:38:24 loizides Exp $
+//
+// None
+//
+// Dummy module to do nothing. Can be a place holder.
+
+// Authors: C.Loizides
+//--------------------------------------------------------------------------------------------------
 
 #include "FWCore/Framework/interface/Frameworkfwd.h"
 #include "FWCore/Framework/interface/EDAnalyzer.h"
@@ -6,16 +14,19 @@
 #include "FWCore/Framework/interface/MakerMacros.h"
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
 
-class None : public edm::EDAnalyzer {
-   public:
-    explicit None(const edm::ParameterSet&) {}
-    ~None() {};
+namespace mitedm
+{
+  class None : public edm::EDAnalyzer {
+    public:
+      explicit None(const edm::ParameterSet&) {}
+      ~None() {};
 
-   private:
-    virtual void beginJob() {}
-    virtual void analyze(const edm::Event&, const edm::EventSetup&) {}
-    virtual void endJob() {}
-};
+     private:
+       virtual void beginJob() {}
+       virtual void analyze(const edm::Event&, const edm::EventSetup&) {}
+       virtual void endJob() {}
+  };
+}
 
 //define this as a plug-in
-DEFINE_FWK_MODULE(None);
+DEFINE_FWK_MODULE(mitedm::None);
