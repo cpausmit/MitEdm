16f28e35b74643777330a04475261cffd706b6f9
edwenger <>
2009-12-07 22:39:07 +0000

    python configs for filtering on event information

---
+++ Filters/python/FilterBX_cff.py
@@ -0,0 +1,16 @@
+import FWCore.ParameterSet.Config as cms
+
+###### Selection on bunch pattern (collisions, empty bunches, parasitic bunch crossings) ######
+
+collisionBunchCrossings = cms.EDFilter("FilterBX",
+                                       crossings = cms.untracked.vint32(51,2724)
+                                       )
+
+emptyBunchCrossings = cms.EDFilter("FilterBX",
+                                   crossings = cms.untracked.vint32(3170,2276)
+                                   )
+
+parasiticBunchCrossings = cms.EDFilter("FilterBX",
+                                       crossings = cms.untracked.vint32(1830,1831,1832,1833)
+                                       )
+
---
+++ Filters/python/FilterHLT_cff.py
@@ -0,0 +1,13 @@
+import FWCore.ParameterSet.Config as cms
+
+###### HLT (https://twiki.cern.ch/twiki/bin/viewauth/CMS/DNDEtaTriggerList#Relevant_HLT_Paths) ######
+import HLTrigger.HLTfilters.hltHighLevel_cfi
+
+#MinBiasBSC
+hltMinBiasBSC = HLTrigger.HLTfilters.hltHighLevel_cfi.hltHighLevel.clone()
+hltMinBiasBSC.HLTPaths = cms.vstring('HLT_MinBiasBSC')
+
+#MinBiasPixel
+hltMinBiasPixel1 = HLTrigger.HLTfilters.hltHighLevel_cfi.hltHighLevel.clone()
+hltMinBiasPixel1.HLTPaths = cms.vstring('HLT_MinBiasPixel_SingleTrack')
+
