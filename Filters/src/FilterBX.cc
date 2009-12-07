0ab661a0af6329884ae6bf4e33a0114e59777ca1
edwenger <>
2009-12-07 22:39:50 +0000

    new filter to select only specified bunch crossings

---
+++ Filters/src/FilterBX.cc
@@ -0,0 +1,59 @@
+//--------------------------------------------------------------------------------------------------
+// $Id: FilterBX.cc,v 1.1 2009/12/05 12:29:50 edwenger Exp $
+//
+// FilterBX
+//
+// Filter to select events from specified bunch crossing numbers
+//
+// Authors: E.Wenger
+//--------------------------------------------------------------------------------------------------
+
+#include "FWCore/ParameterSet/interface/InputTag.h"
+#include "FWCore/Framework/interface/Frameworkfwd.h"
+#include "FWCore/Framework/interface/EDFilter.h"
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "FWCore/Framework/interface/ESHandle.h"
+#include "FWCore/MessageLogger/interface/MessageLogger.h"
+
+namespace mitedm
+{
+  class BunchCrossingFilter : public edm::EDFilter {
+  public:
+    explicit BunchCrossingFilter(const edm::ParameterSet &ps);
+    ~BunchCrossingFilter();
+    
+  protected:
+    virtual bool filter (edm::Event &iEvent, const edm::EventSetup &iSetup);
+    std::vector<int> crossings_;
+  };
+}
+
+using namespace mitedm;
+using namespace edm;
+using namespace std;
+
+//--------------------------------------------------------------------------------------------------
+FilterBX::FilterBX(const edm::ParameterSet& iConfig)
+  : crossings_ = iConfig.getUntrackedParameter< std::vector<int> >("crossings");
+{
+}
+
+//--------------------------------------------------------------------------------------------------
+bool FilterBX::filter( edm::Event &iEvent, const edm::EventSetup &iSetup)
+{
+  // Filter events based on specified bunch crossings
+  
+  bool accepted = false;
+  int bx = iEvent.bunchCrossing();
+
+  for(unsigned int ibunch = 0; ibunch<crossings_.size(); ibunch++) {
+    if(bx == crossings_[ibunch]) accepted = true;
+  }
+  
+  return accepted;
+}
+
+//define this as a plug-in
+DEFINE_FWK_MODULE(FilterBX);
