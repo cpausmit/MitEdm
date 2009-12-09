4254bd9ce007ded1a256a0f8f94a7f25ae0cc781
edwenger <>
2009-12-09 16:39:52 +0000

    refined parameters of loose selection

--- Filters/python/FilterEvtSel_cff.py
+++ Filters/python/FilterEvtSel_cff.py
@@ -2,8 +2,8 @@ import FWCore.ParameterSet.Config as cms
 
 ###### Filter on event selection object (HF energy/timing, pixel hits, cluster shapes ######
 looseEvtSelFilter = cms.EDFilter("FilterEvtSel",
-    clusterPars = cms.untracked.vdouble(0.0,0.008),
-    minHfEnergy = cms.untracked.double(10.0),
-    maxHfTimeDiff = cms.untracked.double(100.0),
+    clusterPars = cms.untracked.vdouble(0.5,0.0045),
+    minHfEnergy = cms.untracked.double(0),
+    maxHfTimeDiff = cms.untracked.double(0),
     srcEvtSel = cms.untracked.string("evtSelData")
 )
