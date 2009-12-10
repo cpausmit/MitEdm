f8d71faf470bd1c68bb9d71eb279735faf5a9300
edwenger <>
2009-12-10 16:01:42 +0000

    use collision types instead of hard-coded BXs

--- Filters/python/FilterBX_cff.py
+++ Filters/python/FilterBX_cff.py
@@ -1,14 +1,16 @@
-# $Id:$
+# $Id: FilterBX_cff.py,v 1.2 2009/12/09 13:55:36 loizides Exp $
 
 import FWCore.ParameterSet.Config as cms
 
 ###### Selection on bunch pattern (collisions, empty bunches, parasitic bunch crossings) ######
 collisionBunchCrossings = cms.EDFilter("FilterBX",
-    crossings = cms.untracked.vint32(51,2724)
+    #crossings = cms.untracked.vint32(51,2724)       # for run 123596
+    coltype = cms.untracked.int32(1)
 )
 
 emptyBunchCrossings = cms.EDFilter("FilterBX",
-    crossings = cms.untracked.vint32(3170,2276)
+    #crossings = cms.untracked.vint32(3170,2276)     # for run 123596
+    coltype = cms.untracked.int32(2)
 )
 
 parasiticBunchCrossings = cms.EDFilter("FilterBX",
