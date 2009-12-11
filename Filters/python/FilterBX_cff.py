66faf2834ea3e3457a4c7bcf35a8f6ac3a176388
loizides <>
2009-12-11 12:23:52 +0000

    missing comma

--- Filters/python/FilterBX_cff.py
+++ Filters/python/FilterBX_cff.py
@@ -1,15 +1,15 @@
-# $Id: FilterBX_cff.py,v 1.3 2009/12/10 16:01:42 edwenger Exp $
+# $Id: FilterBX_cff.py,v 1.4 2009/12/11 12:21:51 loizides Exp $
 
 import FWCore.ParameterSet.Config as cms
 
 ###### Selection on bunch pattern (collisions, empty bunches, parasitic bunch crossings) ######
 collisionBunchCrossings = cms.EDFilter("FilterBX",
-    crossings = cms.untracked.vint32()
+    crossings = cms.untracked.vint32(),
     coltype = cms.untracked.int32(1)
 )
 
 emptyBunchCrossings = cms.EDFilter("FilterBX",
-    crossings = cms.untracked.vint32()
+    crossings = cms.untracked.vint32(),
     coltype = cms.untracked.int32(2)
 )
 
