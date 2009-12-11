3549d56e8e09299edc646c7efe456a397ee070ca
loizides <>
2009-12-11 12:21:51 +0000

    Fix

--- Filters/python/FilterBX_cff.py
+++ Filters/python/FilterBX_cff.py
@@ -1,19 +1,18 @@
-# $Id: FilterBX_cff.py,v 1.2 2009/12/09 13:55:36 loizides Exp $
+# $Id: FilterBX_cff.py,v 1.3 2009/12/10 16:01:42 edwenger Exp $
 
 import FWCore.ParameterSet.Config as cms
 
 ###### Selection on bunch pattern (collisions, empty bunches, parasitic bunch crossings) ######
 collisionBunchCrossings = cms.EDFilter("FilterBX",
-    #crossings = cms.untracked.vint32(51,2724)       # for run 123596
+    crossings = cms.untracked.vint32()
     coltype = cms.untracked.int32(1)
 )
 
 emptyBunchCrossings = cms.EDFilter("FilterBX",
-    #crossings = cms.untracked.vint32(3170,2276)     # for run 123596
+    crossings = cms.untracked.vint32()
     coltype = cms.untracked.int32(2)
 )
 
 parasiticBunchCrossings = cms.EDFilter("FilterBX",
     crossings = cms.untracked.vint32(1830,1831,1832,1833)
 )
-
