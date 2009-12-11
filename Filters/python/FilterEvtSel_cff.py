7b4375b1636de603f4aa5df56015d90d35e62f21
loizides <>
2009-12-11 11:22:31 +0000

    Added nhitsmax

--- Filters/python/FilterEvtSel_cff.py
+++ Filters/python/FilterEvtSel_cff.py
@@ -1,11 +1,12 @@
-# $Id:$
+# $Id: FilterEvtSel_cff.py,v 1.5 2009/12/10 15:31:04 loizides Exp $
 
 import FWCore.ParameterSet.Config as cms
 
 ###### Filter on event selection object (HF energy/timing, pixel hits, cluster shapes ######
 looseEvtSelFilter = cms.EDFilter("FilterEvtSel",
-    clusterPars = cms.untracked.vdouble(0.5,0.0045),
-    minHfEnergy = cms.untracked.double(0),
+    clusterPars   = cms.untracked.vdouble(0.5,0.0045),
+    minHfEnergy   = cms.untracked.double(0),
     maxHfTimeDiff = cms.untracked.double(0),
-    srcEvtSel = cms.untracked.string("evtSelData")
+    nhitsmax      = cms.untracked.int32(0),
+    srcEvtSel     = cms.untracked.string("evtSelData")
 )
