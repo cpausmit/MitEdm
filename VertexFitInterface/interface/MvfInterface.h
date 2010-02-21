a8ae14faf8fe511b55a32ba56049b4473a78aed6
bendavid <>
2010-02-21 23:40:53 +0000

    Add TransientTrack interface for vertex fit

--- VertexFitInterface/interface/MvfInterface.h
+++ VertexFitInterface/interface/MvfInterface.h
@@ -1,5 +1,5 @@
 //--------------------------------------------------------------------------------------------------
-// $Id: MvfInterface.h,v 1.8 2008/09/27 05:48:26 loizides Exp $
+// $Id: MvfInterface.h,v 1.9 2008/11/13 16:36:48 paus Exp $
 //
 // MvfInterface class header file
 //
@@ -12,6 +12,7 @@
 #define MITEDM_VERTEXFITINTERFACE_MVFINTERFACE_H
 
 #include "DataFormats/TrackReco/interface/Track.h"
+#include "TrackingTools/TransientTrack/interface/TransientTrack.h"
 #include "MitCommon/VertexFit/interface/MultiVertexFitter.h"
 #include "MitCommon/VertexFit/interface/MultiVertexFitterC.h"
 #include "MitCommon/VertexFit/interface/MultiVertexFitterD.h"
@@ -32,6 +33,12 @@ namespace mitedm
                     mithep::MultiVertexFitterC::vertexNumber jv);
       bool addTrack(const reco::Track *trk, const int id, const float mass,
                     mithep::MultiVertexFitterD::vertexNumber jv);
+      bool addTrack(const reco::TransientTrack &trk, const int id, const float mass,
+                    mithep::MultiVertexFitter::vertexNumber jv);
+      bool addTrack(const reco::TransientTrack &trk, const int id, const float mass,
+                    mithep::MultiVertexFitterC::vertexNumber jv);
+      bool addTrack(const reco::TransientTrack &trk, const int id, const float mass,
+                    mithep::MultiVertexFitterD::vertexNumber jv);                    
 
       mithep::MultiVertexFitter  *fitter () { return mvf_;}
       mithep::MultiVertexFitterC *fitterC() { return mvfC_;}
--- VertexFitInterface/src/MvfInterface.cc
+++ VertexFitInterface/src/MvfInterface.cc
@@ -1,4 +1,4 @@
-// $Id: MvfInterface.cc,v 1.8 2008/11/13 16:36:48 paus Exp $
+// $Id: MvfInterface.cc,v 1.9 2009/12/07 16:27:37 mrudolph Exp $
 
 #include "MitEdm/VertexFitInterface/interface/MvfInterface.h"
 #include "MitEdm/VertexFitInterface/interface/TrackParameters.h"
@@ -57,6 +57,39 @@ bool MvfInterface::addTrack(const Track *trk, const int id, const float mass,
   // Add a track to the track pool
 
   TrackParameters cmsTrk(trk,iCms, mvfD_->bField());
+  TrackParameters mvfTrk = cmsTrk.mvfTrack(); 
+  return (mvfD_->addTrack(*mvfTrk.pars(),*mvfTrk.cMat(),id,mass,jv));
+}
+
+//--------------------------------------------------------------------------------------------------
+bool MvfInterface::addTrack(const TransientTrack &trk, const int id, const float mass,
+                            MultiVertexFitter::vertexNumber jv)
+{
+  // Add a track to the track pool
+
+  TrackParameters cmsTrk(trk,iCms);
+  TrackParameters mvfTrk = cmsTrk.mvfTrack(); 
+  return (mvf_->addTrack(*mvfTrk.pars(),*mvfTrk.cMat(),id,mass,jv));
+}
+
+//--------------------------------------------------------------------------------------------------
+bool MvfInterface::addTrack(const TransientTrack &trk, const int id, const float mass,
+                            MultiVertexFitterC::vertexNumber jv)
+{
+  // Add a track to the track pool
+
+  TrackParameters cmsTrk(trk,iCms);
+  TrackParameters mvfTrk = cmsTrk.mvfTrack(); 
+  return (mvfC_->addTrack(*mvfTrk.pars(),*mvfTrk.cMat(),id,mass,jv));
+}
+
+//--------------------------------------------------------------------------------------------------
+bool MvfInterface::addTrack(const TransientTrack &trk, const int id, const float mass,
+                            MultiVertexFitterD::vertexNumber jv)
+{
+  // Add a track to the track pool
+
+  TrackParameters cmsTrk(trk,iCms);
   TrackParameters mvfTrk = cmsTrk.mvfTrack(); 
   return (mvfD_->addTrack(*mvfTrk.pars(),*mvfTrk.cMat(),id,mass,jv));
 }
