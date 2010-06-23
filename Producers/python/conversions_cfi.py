c399c9c12d812bf889810c0e35b8120e55664754
bendavid <>
2010-06-23 13:13:33 +0000

    Updated conversion sequences

--- Producers/python/conversions_cfi.py
+++ Producers/python/conversions_cfi.py
@@ -1,4 +1,4 @@
-# $Id: conversions_cfi.py,v 1.9 2010/06/08 20:17:29 bendavid Exp $
+# $Id: conversions_cfi.py,v 1.10 2010/06/09 00:06:20 bendavid Exp $
 
 import FWCore.ParameterSet.Config as cms
 
@@ -21,5 +21,18 @@ mvfTrackerConversions = mvfConversions.clone(
 mvfConversionRemoval = mvfConversions.clone(
     iStables1        = cms.untracked.string('gsfElectronsStable'),
     iStables2        = cms.untracked.string('mergedElectronsStable'),
+    useRhoMin        = cms.untracked.bool(False),
     applyChargeConstraint = cms.untracked.bool(True),
+)
+
+kinematicConversions = cms.EDProducer("ProducerConversionsKinematic",
+    iStables1        = cms.untracked.string('mergedElectronsStable'),
+    iStables2        = cms.untracked.string('mergedElectronsStable'),
+    iPVertexes       = cms.untracked.string('offlinePrimaryVerticesWithBS'),
+    oPid             = cms.untracked.int32(22),
+    usePVertex       = cms.untracked.bool(True),
+    convConstraint   = cms.untracked.bool(False),
+    convConstraint3D = cms.untracked.bool(True),
+    rhoMin           = cms.untracked.double(0.9),
+    useRhoMin        = cms.untracked.bool(True),
 )
