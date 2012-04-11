344b47dedc7b6d27d12033b32f162b6d03a3213c
arapyan <>
2012-04-11 16:30:21 +0000

    *** empty log message ***

--- GBRTrain/test/rootlogon.C
+++ GBRTrain/test/rootlogon.C
@@ -1,28 +1,24 @@
-// --> >// $Id: rootlogon.C,v 1.3 2008/06/11 13:26:04 loizides Exp $
+// --> >// $Id: rootlogon.C,v 1.1 2012/04/10 13:38:32 bendavid Exp $
 {
+ {
   TString libstr(Form("%s/lib/%s/%s",
                       gSystem->Getenv("CMSSW_BASE"),
                       gSystem->Getenv("SCRAM_ARCH"),
                       "libCondFormatsEgammaObjects.so"));
 
   gSystem->Load(libstr);
-}
-
-{
-
+ }
+ {
   TString libstr(Form("%s/lib/%s/%s",
                       gSystem->Getenv("CMSSW_BASE"),
                       gSystem->Getenv("SCRAM_ARCH"),
                       "libMitEdmGBRTrain.so"));
 
   gSystem->Load(libstr);
-
-}
-
+ }
   gSystem->AddIncludePath("-I$ROOFITSYS/include");
   gSystem->AddIncludePath("-I$CMSSW_BASE/src/CondFormats/EgammaObjects/interface");
   gSystem->AddIncludePath("-I$CMSSW_BASE/src/MitEdm/GBRTrain/interface");
-
 
   gInterpreter->AddIncludePath((TString(":")+TString(gSystem->Getenv("CMSSW_BASE"))+
 				TString("/src/CondFormats/EgammaObjects/interface")).Data());
